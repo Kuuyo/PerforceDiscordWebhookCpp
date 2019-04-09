e474e56bbde50688c9e7468f82bbe0a328acf7a9
Elias Hernes <brokenkyuu@gmail.com>
2019-04-09 16:57:13 +0200

    Let's test this

--- PerforceDiscordWebhookCpp/PerforceDiscordWebhookCpp.cpp
+++ PerforceDiscordWebhookCpp/PerforceDiscordWebhookCpp.cpp
@@ -4,18 +4,62 @@
 #include "pch.h"
 #include <iostream>
 
+#include "include/p4/clientapi.h"
+
+#ifdef _WIN32
+#pragma comment(lib, "libclient.lib")
+#pragma comment(lib, "librpc.lib")
+#pragma comment(lib, "libsupp.lib")
+#pragma comment(lib, "SSLStub.lib")
+#else
+#pragma comment(lib, "libclient.a")
+#pragma comment(lib, "librpc.a")
+#pragma comment(lib, "libsupp.a")
+#endif
+
 int main()
 {
-    std::cout << "Hello World!\n"; 
+	ClientUser ui;
+	ClientApi client;
+	StrBuf msg;
+	Error e;
+
+	// Any special protocol mods
+
+	//client.SetProtocol( "tag", "");
+
+	// Connect to server
+
+#ifndef _WIN32
+	std::cout << getenv("P4USER") << std::endl;
+#endif
+
+	client.Init(&e);
+
+	std::cout << client.GetUser().Text() << std::endl;
+
+	if (e.Test())
+	{
+		e.Fmt(&msg);
+		fprintf(stderr, "%s\n", msg.Text());
+		return 1;
+	}
+
+
+	char    *argv[] = {  (char*)"-l", (char*)"-m", (char*)"5", (char*)"-s", (char*)"submitted", (char*)"-t" };
+	int     argc = 6;
+	client.SetArgv(argc, argv);
+	client.Run("changes", &ui);
+	// Close connection
+
+	client.Final(&e);
+
+	if (e.Test())
+	{
+		e.Fmt(&msg);
+		fprintf(stderr, "%s\n", msg.Text());
+		return 1;
+	}
+
+	return 0;
 }
-
-// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
-// Debug program: F5 or Debug > Start Debugging menu
-
-// Tips for Getting Started: 
-//   1. Use the Solution Explorer window to add/manage files
-//   2. Use the Team Explorer window to connect to source control
-//   3. Use the Output window to see build output and other messages
-//   4. Use the Error List window to view errors
-//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
-//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
